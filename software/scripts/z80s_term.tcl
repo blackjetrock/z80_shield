a91abfd26cccc2cbe7f42fc79f7831fa60934f97
blackrockjet <menadue@gmail.com>
2019-10-22 06:24:19 +0100

    Added ihx upload to terminal emulator. Seems to work. The upload menu option must be selected before the upload is started using the menu option.

--- software/scripts/z80s_term.tcl
+++ software/scripts/z80s_term.tcl
@@ -75,17 +75,120 @@ proc open_data_channel_window {w} {
 
 ####################################################################################################
 #
-# Sends an IHX file to the Mega
+# Send .IHX file to mega
 #
+
+proc send_ihx_file {filename device} {
+    
+    # Read the hex file
+    
+    set f [open $filename]
+    set txt [read $f]
+    close $f
+    
+    # Open the hex file and send it to the sketch
+    puts "Opening $device"
+    set f [open $device r+]
+    #set f [open /dev/tty  r+]
+    #fconfigure $f -mode 9600,n,8,1
+    #fconfigure $f -handshake none
+    fconfigure $f -blocking 0
+    
+    if { 0 } {
+	while { 1 } {
+	    puts $f "S\n"
+	    set d [read $f]
+	    if { [string length $d] > 0 } {
+		puts $d
+	    }
+	}
+    }
+    
+    # Flush
+    puts "Flushing..."
+    set done 0
+    while { !$done } {
+	set tx [read $f]
+	if { [string length $tx] == 0 } {
+	    set done 1
+	} else {
+	    puts $tx
+	}
+    }
+    
+    
+    # Signal that we have started
+    puts "Starting"
+    
+    puts $f "S"
+    flush $f
+    
+    after 2000
+    
+    # Flush
+    puts "Flushing..."
+    set done 0
+    while { !$done } {
+	set tx  [read $f]
+	if { [string length $tx ] == 0 } {
+	    set done 1
+	} else {
+	    puts "Flushed '$tx '"
+	}
+	
+	
+    }
+    
+    puts "Sending $filename"
+    
+    foreach line [split $txt "\n"] {
+	if { [string length $line] == 0 } {
+	    break
+	}
+	
+	# We terminate line with a '-'
+	puts "Sending line '$line'"
+	puts $f "$line-"
+	flush $f
+	
+	# Wait for a '+' that signals the next line can be sent
+	set rx_tick 0
+	puts "Waiting for tick"
+	
+	while {!$rx_tick} {
+	    
+	    set rdata [read $f]
+	    
+	    if { [string length $rdata] > 0 } {
+		if { [string first "+" $rdata] != -1 } {
+		    puts "Got '$rdata'"
+		    # Tick received, move on
+		    set rx_tick 1
+		    
+		}
+	    }
+	}
+    }
+}
+
+####################################################################################################
+#
+# Sends an IHX file to the Mega
+# Requires upload menu to be selected before executing 
 #
 
 proc send_ihx_file_dialog {} {
+    global device
+    
     set types {
 	{{IHX Files} {.ihx}}
     }
     
     set filename [tk_getOpenFile -initialdir ../z80_c -filetypes $types ]
 
+    if { $filename != "" } {
+	send_ihx_file $filename $device
+    }
 }
 
 
